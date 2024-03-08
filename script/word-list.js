2b5c3fa1a4b687e80891da0d96092832aa87e113
Youssef-joe <joe92228@gmail.com>
2024-03-08 20:35:01 +0200

    initial commit

---
+++ script/word-list.js
@@ -0,0 +1,18 @@
+const wordList = [
+   {
+    word:'fatma',
+    hint: 'the one who made a great cute presentation'
+   },
+   {
+    word:'youssef',
+    hint:'the one who made this game'
+   },
+   {
+    word: 'zoza',
+    hint: 'your tocix sister'
+   },
+   {
+    word: 'aya',
+    hint: 'your farmer sister'
+   }
+]
